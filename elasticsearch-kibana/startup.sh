7a5d5b4adb51b2ed7042e58a679b3033c2af342f
haalcala <harold.dev@gmail.com>
2020-06-22 23:39:20 +0800

    Startup order

--- elasticsearch-kibana/startup.sh
+++ elasticsearch-kibana/startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+service elasticsearch start
+
+sleep 30
+
+runuser -u kibana kibana-6.8.10-linux-x86_64/bin/kibana &
+
+sleep 20
+
 metricbeat setup --dashboards
 
 service metricbeat start
-service elasticsearch start
 
-sleep 10
-
-runuser -u kibana kibana-6.8.10-linux-x86_64/bin/kibana
+sleep Infinity 
